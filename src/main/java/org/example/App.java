67188682c1b0e241ea46644c55a3b99ae088498d
Дмитрий Швалев <m2109465@edu.misis.ru>
2023-05-05 15:15:44 +0300

    initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,41 @@
+package org.example;
+
+import org.example.model.Item;
+import org.example.model.Person;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class App
+{
+    public static void main( String[] args ) {
+        Configuration configuration = new Configuration().addAnnotatedClass(Person.class).addAnnotatedClass(Item.class);
+
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+        Session session = sessionFactory.getCurrentSession();
+
+        try {
+            session.beginTransaction();
+
+            Person person = new Person("Test cascading 2", 23);
+            person.addItem(new Item("item 1"));
+            person.addItem(new Item("item 2"));
+            person.addItem(new Item("item 3"));
+
+            session.persist(person);
+
+
+            session.getTransaction().commit();
+
+
+        } finally {
+            sessionFactory.close();
+        }
+
+
+    }
+}
